a448ffebaa91fa21d5c113831eb38ab5ad29d476
ousttrue <ousttrue@gmail.com>
2022-11-28 19:34:26 +0900

    packages.toml

--- toprefix/packages.py
+++ toprefix/packages.py
@@ -1,47 +1,80 @@
 from typing import Optional, List
 from .package import Pkg, MesonPkg, CMakePkg, MakePkg, PrebuiltPkg
-from .package.source import Archive, GitRepository
+from .package.source import Source, Archive, GitRepository
+import pkgutil
+import toml
 
-PKGS: List[Pkg] = [
-    MesonPkg(Archive.gnome(name="glib", major=2, minor=75, patch=0)),
-    MesonPkg(Archive.gnome(name="gtk", major=4, minor=8, patch=2)),
-    MesonPkg(Archive.gnome(name="gobject-introspection", major=1, minor=74, patch=0)),
-    MesonPkg(Archive.gnome(name="pygobject", major=3, minor=42, patch=0)),
-    MesonPkg(GitRepository.github("wizbright", "waybox")),
-    MesonPkg(GitRepository.github("labwc", "labwc")),
-    MesonPkg(
-        Archive.from_url(
-            "https://gitlab.freedesktop.org/mesa/drm/-/archive/libdrm-2.4.114/drm-libdrm-2.4.114.tar.bz2"
+PKGS: List[Pkg] = []
+
+
+def make_pkg(pkg: str, source: Source) -> Pkg:
+    match pkg:
+        case "meson":
+            return MesonPkg(source)
+        case "prebuilt":
+            return PrebuiltPkg(source)
+        case _:
+            raise NotImplementedError(pkg)
+
+
+def pkgs_url(items):
+    for item in items:
+        yield make_pkg(item["pkg"], Archive.from_url(item["url"]))
+
+
+def pkgs_gnome(items):
+    for item in items:
+        source = Archive.gnome(
+            name=item["name"],
+            major=item["major"],
+            minor=item["minor"],
+            patch=item["patch"],
         )
-    ),
-    # TODO: move prefix/share/pkgconfig/wayland-protocols.pc to lib64/pkgconfig
-    MesonPkg(
-        Archive.from_url(
-            "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/archive/1.29/wayland-protocols-1.29.tar.bz2"
-        )
-    ),
-    MesonPkg(Archive.github_tag("MusicPlayerDaemon", "MPD", "v0.23.10")),
-    MesonPkg(
-        Archive.codeberg_tag(
-            "dnkl",
-            "foot",
-            "1.13.1",
-        )
-    ),
-    MesonPkg(
-        Archive.from_url(
-            "https://github.com/harfbuzz/harfbuzz/releases/download/5.3.1/harfbuzz-5.3.1.tar.xz"
-        )
-    ),
-    MesonPkg(Archive.codeberg_tag("dnkl", "tllist", "1.1.0")),
-    PrebuiltPkg(Archive.from_url("https://go.dev/dl/go1.19.3.linux-amd64.tar.gz")),
-    PrebuiltPkg(Archive.github_tag("junegunn", "fzf", "0.35.1")),
-    PrebuiltPkg(
-        Archive.from_url(
-            "https://github.com/x-motemen/ghq/releases/download/v1.3.0/ghq_linux_amd64.zip"
-        )
-    ),
-]
+        yield make_pkg(item["pkg"], source)
+
+
+def pkgs_github(items):
+    for item in items:
+        if "tag" in item:
+            source = Archive.github_tag(item["user"], item["name"], item["tag"])
+        else:
+            source = GitRepository.github(item["user"], item["name"])
+        yield make_pkg(item["pkg"], source)
+
+
+def pkgs_codeberg(items):
+    for item in items:
+        if "tag" in item:
+            source = Archive.codeberg_tag(item["user"], item["name"], item["tag"])
+        else:
+            raise NotImplementedError()
+        yield make_pkg(item["pkg"], source)
+
+
+def generate_pkgs(parsed):
+    for k, v in parsed.items():
+        match k:
+            case "url":
+                for pkg in pkgs_url(v):
+                    yield pkg
+            case "gnome":
+                for pkg in pkgs_gnome(v):
+                    yield pkg
+            case "github":
+                for pkg in pkgs_github(v):
+                    yield pkg
+            case "codeberg":
+                for pkg in pkgs_codeberg(v):
+                    yield pkg
+            case _:
+                raise NotImplementedError(k)
+
+
+data = pkgutil.get_data("toprefix", "assets/packages.toml")
+if data:
+    parsed = toml.loads(data.decode("utf-8"))
+    for pkg in generate_pkgs(parsed):
+        PKGS.append(pkg)
 
 
 def list_pkgs():
